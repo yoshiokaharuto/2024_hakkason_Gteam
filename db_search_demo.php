63863faf2928e1b7ad2955d8e4e155ba3552ae94
sawadamasaki <m.sawada.sys23@morijyobi.ac.jp>
2024-09-12 10:12:59 +0900

    検索機能の実装 close #4

---
+++ db_search_demo.php
@@ -0,0 +1,28 @@
+<?php
+
+//ユーザ名
+$user = 'root';
+//パスワード
+$password = '';
+//データベース名
+$database = 'hakkason_gteam';
+//サーバ名
+$server = 'localhost:3308';
+
+//DSN文字列
+$dsn = "mysql:host={$server};dbname={$database};charset=utf8";
+
+//mysqlへの接続
+try {
+    $pdo =  new PDO($dsn, $user, $password);
+    
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, false);
+
+    //例外
+    $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Error: ' . $e -> getMessage();
+    die();
+}
+
+?>
